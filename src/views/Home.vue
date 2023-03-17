9a08abdc9670566af67a8eb326fda1830f348958
Neymar998 <87710493+Neymar998@users.noreply.github.com>
2023-03-18 00:32:35 +0800

    2023.3.18 0:32

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,21 @@
 <template>
-    <div class="container">
+    <div class="home">
         <Blogpost :props="welcomScreen" />
         <Blogpost :props="prop" v-for="(prop, index) in sampleBlogPost" :key="index" />
+        <div class="blog-card-wrap">
+            <div class="container">
+                <h3>View More Recent Card</h3>
+                <div class="blog-card">
+                    <Blogcard v-for="(post, index) in sampleBlogCard" :key="index" />
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
 <script setup>
 import Blogpost from '../components/Blogpost.vue';
+import Blogcard from '../components/Blogcard.vue';
 import { reactive } from 'vue'
 let welcomScreen = reactive({
     title: 'Welcome!',
@@ -23,6 +32,11 @@ let sampleBlogPost = reactive([{
     blogHTML: 'bbbbbbbbbbbbbbb',
     blogCoverPhoto: 'designed-for-everyone'
 }])
+let sampleBlogCard = [
+    { blogTitle: "Blog Card #1", blogCoverPhoto: "stock-1", blogDate: "May,1,2023" },
+    { blogTitle: "Blog Card #2", blogCoverPhoto: "stock-2", blogDate: "May,2,2023" },
+    { blogTitle: "Blog Card #3", blogCoverPhoto: "stock-3", blogDate: "May,3,2023" }
+]
 </script>
 
 <style lang="scss" scoped></style>
